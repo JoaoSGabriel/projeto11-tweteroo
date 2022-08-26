4012d5a56c51784541565baf9aaa337ef6fd3356
JoaoSGabriel <joaogabriel0359@gmail.com>
2022-08-25 21:22:18 -0300

    implement function to create account and write tweet

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 import express from 'express';
+import cors from 'cors';
 
 const server = express();
+server.use(cors());
+server.use(express.json());
 
-server.listen(5000, () => {
-    console.log('Listen on 5000');
+const accounts = [];
+const twetts = [
+    {username: "bobesponja",
+    avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+    tweet: "eu amo o hub"}
+];
+
+server.post('/sign-up', (req, res) => {
+    const user = req.body;
+    accounts.push({username: user.username, avatar: user.avatar});
+    res.send("OK");
 });
+
+server.post('/tweets', (req, res) => {
+    const newTweet = req.body;
+
+    twetts.push({username: newTweet.username, tweet: newTweet.tweet});
+    res.send("OK");
+})
+
+server.get('/tweets', (req, res) => {
+    res.send(twetts);
+});
+
+
+server.listen(5000, () => console.log('Listen on 5000'));
