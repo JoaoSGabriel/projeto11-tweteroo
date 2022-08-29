b809c6fec0030bb828cb1326f26c0aec5febfd3e
JoaoSGabriel <joaogabriel0359@gmail.com>
2022-08-29 11:30:52 -0300

    fix the post route with bonus

--- index.js
+++ index.js
@@ -7,15 +7,30 @@ server.use(express.json());
 
 const accounts = [];
 const twetts = [
-    {username: "bobesponja",
-    avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-    tweet: "eu amo o hub"}
+    {username: "Michael Scott",
+    avatar: "https://cdn.mensagenscomamor.com/content/images/m000575215.jpg?v=1&w=480&h=480",
+    tweet: "I'm dead inside"},
+    {username: "Jim Halpert",
+    avatar: "https://rollingstone.uol.com.br/media/uploads/john-krasinski-the-office_reproducao.jpg",
+    tweet: "Not a bad day"},
+    {username: "Pam Beesly",
+    avatar: "https://i.pinimg.com/originals/b4/37/0b/b4370be92a23d01d414d94983c2fb925.jpg",
+    tweet: "I hate the idea that someone out there hates me."},
+    {username: "Ryan Howard",
+    avatar: "https://br.web.img3.acsta.net/r_1280_720/newsv7/20/08/20/22/34/26755250.jpg",
+    tweet: "I hooked up with her on February 13th"},
+    {username: "dwight Schrute",
+    avatar: "https://rollingstone.uol.com.br/media/_versions/rainn_wilson_como_dwight_schrute_em_the_office_foto__reproducao_via_imdb_widelg.jpg",
+    tweet: "Today, smokes will save lives"}
 ];
 
 server.post('/sign-up', (req, res) => {
     const { username, avatar } = req.body;
     if (!username || !avatar) {
         return res.status(400).send({error: "Envie todos os campos"})
+    }
+    if (accounts.find(value => value.username === username)) {
+        return res.status(400).send({error: "Esta conta já existe!"})
     }
     accounts.push({
         username: username,
@@ -24,7 +39,8 @@ server.post('/sign-up', (req, res) => {
 });
 
 server.post('/tweets', (req, res) => {
-    const { username, tweet } = req.body;
+    const username = req.headers.username
+    const { tweet } = req.body;
     if (!username || !tweet) {
         return res.status(400).send({error: "Envie todos os campos"})
     }
@@ -45,7 +61,10 @@ server.get('/tweets', (req, res) => {
         res.send(tweetServer);
         return;
     }
-    res.send(twetts);
+    for (let i = twetts.length -1; i >= 0; i--) {
+        tweetServer.push(twetts[i]);
+    }
+    res.send(tweetServer);
 });
 
 
